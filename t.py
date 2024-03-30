611a2db0ecd06d29f154c09e5cb91d334efcebde
BrandonTay2001 <brandontay2001@gmail.com>
2024-03-30 23:56:25 +0800

    new test

--- t.py
+++ t.py
@@ -1,16 +1,25 @@
-import random
-import bittensor as bt
-from storage.api import store, retrieve
 import asyncio
-
-# Store data
-wallet = bt.wallet()
-subtensor = bt.subtensor()
+from storage import StoreUserAPI
+import bittensor as bt
 
 async def main():
-    data = b"Some bytestring data!"
-    cid, hotkeys = await store(data, wallet, subtensor, netuid=22)
-    print("Stored {} with {} hotkeys".format(cid, hotkeys))
+    # Load the wallet desired
+    wallet = bt.wallet()
+    store = StoreUserAPI(wallet)
+    # Fetch the subnet 21 validator set via metagraph
+    metagraph = bt.metagraph(netuid=21)
 
-# Run the async function
+    # Store data on the decentralized network!
+    cid = await store(
+       metagraph=metagraph,
+       # add any arguments for the `StoreUser` synapse
+       data=b"some data", # Any data (must be bytes) to store
+       encrypt=True, # encrpyt the data using the bittensor wallet provided
+       ttl=60 * 60 * 24 * 30,
+       encoding="utf-8",
+       uid=None, # query a specific validator UID if desired
+    )
+
+    print(cid)
+
 asyncio.run(main())
